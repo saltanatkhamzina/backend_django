8b296f98ae2e8a6779cafdf0ae4a73e98985c86f
mooneex <116973490+yusennn@users.noreply.github.com>
2023-12-01 21:14:55 +0600

    da

--- cafe/tests.py
+++ cafe/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import MagicMock
+from .management.commands.run_telegram_bot import start, foods, handle_help
 
-# Create your tests here.
+
+class TestBot(unittest.TestCase):
+
+    def test_start(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        start(message)
+
+    def test_help(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        handle_help(message)
+
+    def test_foods(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        foods(message)
+
+
+if __name__ == '__main__':
+    unittest.main()
